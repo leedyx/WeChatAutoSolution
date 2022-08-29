856af1c60dba17ed850b41895821426140026ea4
leedyx1990 <leedyx1990@163.com>
2022-08-29 19:45:53 +0800

    修改版本号 自动下载

--- WeChatCore/core.cpp
+++ WeChatCore/core.cpp
@@ -5,109 +5,17 @@ DWORD GetWeChatWinBase() {
 	return (DWORD)GetModuleHandleA("WeChatWin.dll");
 }
 
-DWORD jmpBackAddress = 0;
+VOID ChangeVersion(DWORD latestVersion) {
+	
+	DWORD base = GetWeChatWinBase();
+	LPDWORD address = (LPDWORD)(base + CHANGE_VERSION_OFFSET);
+	*(DWORD*)address = latestVersion;
 
-VOID SaveDuiLibXML(BYTE* xmlData, DWORD xmlSize, WCHAR* xmlPathFileName)
-{
-	std::wstring filepath = L"D:\\WeChatDuiLibXMLFile";
-	if (!PathIsDirectory(filepath.c_str()))
-	{
-		CreateDirectory(filepath.c_str(), NULL);
-	}
-
-	std::wstring wsXmlPathFileName = xmlPathFileName;
-	int index = wsXmlPathFileName.find(L"/");
-	if (index < 0) index = wsXmlPathFileName.find(L"\\");
-
-	if (index > 0)
-	{
-		std::wstring wsXmlPath = wsXmlPathFileName.substr(0, index);
-		filepath += L"\\" + wsXmlPath;
-		if (!PathIsDirectory(filepath.c_str()))
-		{
-			CreateDirectory(filepath.c_str(), NULL);
-		}
-	}
-
-	std::wstring wsXmlFileName = wsXmlPathFileName.substr(index + 1);
-	filepath += L"\\" + wsXmlFileName;
-
-	OutputDebugString(filepath.c_str());
-
-	HANDLE hFile = CreateFile(filepath.c_str(),
-		GENERIC_WRITE, 0, NULL,
-		CREATE_NEW, FILE_ATTRIBUTE_NORMAL, 0);
-
-	DWORD dwBytesWriten = 0;
-	WriteFile(hFile, xmlData, xmlSize * 2, &dwBytesWriten, NULL);
-
-	CloseHandle(hFile);
 }
 
-__declspec(naked) VOID ListenDuiLibXML()
-{
-	_asm
-	{
-		mov ebx, dword ptr ss : [ebp - 0x4];
-		xor ecx, ecx;
+VOID AutoLoadImage() {	
+	char settime[] = "00:00-00:00";
+	DWORD AutoDownloadTimeSettingAddr = GetWeChatWinBase() + AUTO_DOWNLOAD_IMG_OFFSET;
+	WriteProcessMemory(GetCurrentProcess(), (LPVOID)AutoDownloadTimeSettingAddr, settime, strlen(settime) + 1, 0);
 
-		pushfd;
-		pushad;
-
-		mov eax, [ebp];
-		push[eax + 0x8];
-		push edx;
-		push[ebx];
-		call SaveDuiLibXML;
-		add esp, 0xC;
-
-		popad
-			popfd
-
-			jmp jmpBackAddress
-	}
-}
-
-/*
-* 对任意地址添加HOOK
-* dwHookAddr：HOOK的目标地址
-* dwJmpAddress：跳转到的地址
-* originalRecieveCode：保存旧指令的数组
-* return：void
-*/
-void HookAnyAddress(DWORD dwHookAddr, LPVOID dwJmpAddress, char* originalRecieveCode)
-{
-	//组装跳转数据
-	BYTE jmpCode[5] = { 0 };
-	jmpCode[0] = 0xE9;
-
-	//计算偏移
-	*(DWORD*)&jmpCode[1] = (DWORD)dwJmpAddress - dwHookAddr - 5;
-
-	// 保存以前的属性用于还原
-	DWORD OldProtext = 0;
-
-	// 因为要往代码段写入数据，又因为代码段是不可写的，所以需要修改属性
-	VirtualProtect((LPVOID)dwHookAddr, 5, PAGE_EXECUTE_READWRITE, &OldProtext);
-	ReadProcessMemory(GetCurrentProcess(), (LPVOID)dwHookAddr, originalRecieveCode, 5, 0);
-
-	//写入自己的代码
-	memcpy((void*)dwHookAddr, jmpCode, 5);
-
-	// 执行完了操作之后需要进行还原
-	VirtualProtect((LPVOID)dwHookAddr, 5, OldProtext, &OldProtext);
-}
-
-/*
-* 对任意地址取消HOOK
-* dwHookAddr：HOOK的目标地址
-* originalRecieveCode：保存旧指令的数组
-* return：void
-*/
-void UnHookAnyAddress(DWORD dwHookAddr, char* originalRecieveCode)
-{
-	DWORD OldProtext = 0;
-	VirtualProtect((LPVOID)dwHookAddr, 5, PAGE_EXECUTE_READWRITE, &OldProtext);
-	WriteProcessMemory(GetCurrentProcess(), (LPVOID)dwHookAddr, originalRecieveCode, 5, 0);
-	VirtualProtect((LPVOID)dwHookAddr, 5, OldProtext, &OldProtext);
 }
---
+++ WeChatCore/dllmain.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "core.h"
+
+// dllmain.cpp : 定义 DLL 应用程序的入口点。
+
+#include "pch.h"
+#include <Windows.h>
+
+VOID DoAction();
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		//##########################################################
+		//
+		//注意：仅适配PC微信2.6.7.57版本，其它版本不可用
+		//
+		//##########################################################
+		HANDLE hANDLE = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DoAction, NULL, NULL, 0);
+		if (hANDLE != 0)
+		{
+			CloseHandle(hANDLE);
+		}
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+VOID DoAction() {
+	AutoLoadImage();
+	ChangeVersion(1661404701);
+
+}
